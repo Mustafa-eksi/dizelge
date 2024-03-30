dfccf9d39f3e3bd05fb3b9213580066cf504fbdd
mustafa <mustafa.eskieksi@gmail.com>
2024-03-30 17:28:20 +0300

    Added basic parsing and listing.

--- desktop.cpp
+++ desktop.cpp
@@ -1,21 +1,41 @@
+#include <exception>
 #include <ios>
 #include <optional>
+#include <stdexcept>
 #include <string>
 #include <fstream>
 #include <stdio.h>
 #include <unordered_map>
+#include <vector>
 
 namespace deskentry {
 
+// map of groups and their key-values.
+typedef std::unordered_map<std::string, std::unordered_map<std::string, std::string>> UnparsedEntry;
+
+enum ValueType {
+        String,
+        LocaleString,
+        IconString,
+        Boolean,
+        Numeric
+};
+
+struct ParsedValue {
+        ValueType type;
+        std::string strval, locale;
+        bool isArray, isLocal;
+};
+
+typedef std::unordered_map<std::string, ParsedValue> ParsedGroup;
+typedef std::unordered_map<std::string, ParsedGroup> ParsedEntry;
+
 typedef struct DesktopEntry {
-        std::string filename, app_name, exec_command;
+        ParsedEntry pe;
         bool onMenu, onDesktop, onTaskbar;
 } DesktopEntry;
 
-// map of groups and their key-values.
-typedef std::unordered_map<std::string, std::unordered_map<std::string, std::string>> UnparsedDe;
-
-void print_unde(UnparsedDe unde) {
+void print_unde(UnparsedEntry unde) {
         for (auto const& [group, keyval] : unde)
         {
                 printf("Group: %s\n", group.c_str());
@@ -25,9 +45,72 @@ void print_unde(UnparsedDe unde) {
         }
 }
 
-std::optional<DesktopEntry> parse_file(std::string filepath) {
+std::string type_to_str(ValueType t) {
+        switch(t) {
+                case IconString: return "IconString";
+                case LocaleString: return "LocaleString";
+                case String: return "String";
+                case Boolean: return "Boolean";
+                case Numeric: return "Numeric";
+        }
+}
+
+void print_pe(ParsedEntry pe) {
+        for (auto const& [group, pg] : pe)
+        {
+                printf("Group: %s\n", group.c_str());
+                for (auto const& [pkey, pval] : pg) {
+                        printf("\t'%s' = '%s' %s =\"%s\"\n", pkey.c_str(), type_to_str(pval.type).c_str(), pval.isArray ? "(array)" : "",
+                                pval.type == ValueType::String || pval.type == ValueType::LocaleString ? pval.strval.c_str() : "");
+                }
+        }
+}
+
+std::optional<ParsedEntry> parse_entry(UnparsedEntry unde) {
+        if (!unde.contains("Desktop Entry"))
+                return {};
+        ParsedEntry pe;
+
+        for (auto const& [group, keyval] : unde) {
+                pe[group] = {};
+                for (auto const& [key, val] : keyval) {
+                        if (key.ends_with("]")) {
+                                pe[group][key].isLocal = true;
+                                pe[group][key].locale = key.substr(key.find("["));
+                                pe[group][key].type = ValueType::LocaleString;
+                        }
+                        pe[group][key].strval = val;
+
+                        if (val == "true" || val == "false") {
+                                pe[group][key].type = ValueType::Boolean;
+                                pe[group][key].strval = val == "true";
+                                continue;
+                        }
+                        if(key != "Version") {
+                                try {
+                                        float f = std::stof(val);
+                                        pe[group][key].type = ValueType::Numeric;
+                                        continue;
+                                } catch (std::exception e) {}
+                        }
+
+                        if (val.find(";") != std::string::npos) {
+                                pe[group][key].isArray = true;
+                        }
+                        if (pe[group][key].type != ValueType::LocaleString) {
+                                // IconString is only for Icon
+                                pe[group][key].type = ValueType::String;
+                                if (key == "Icon")
+                                        pe[group][key].type = ValueType::IconString;
+                        }
+                }
+        }
+        return pe;
+}
+
+std::optional<UnparsedEntry> read_file(std::string filepath) {
         std::string buffer;
-        UnparsedDe unde;
+        UnparsedEntry unde;
 
         std::ifstream de_file(filepath);
         de_file.seekg(0, std::ios::end);
@@ -41,7 +124,6 @@ std::optional<DesktopEntry> parse_file(std::string filepath) {
                 int end_of_line = buffer.find("\n", i);
 
                 std::string current_line = buffer.substr(i, end_of_line-i);
-                //printf("current line: '%s'\n", current_line.c_str());
                 // Shebang or comment
                 if (current_line.starts_with("#"))
                         goto skip;
@@ -69,16 +151,27 @@ std::optional<DesktopEntry> parse_file(std::string filepath) {
                                 value = value.substr(1);
                         }
                         unde[current_group][key] = value;
-                        //printf("Key: '%s' = Value: '%s'\n", key.c_str(), value.c_str());
                 }
 skip:
                 if (end_of_line < buffer.size())
                         i = end_of_line;
         }
-        print_unde(unde);
-        printf("Ended\n");
+        //print_unde(unde);
 
-        return {};
+        return unde;
+}
+
+std::optional<DesktopEntry> parse_file(std::string filepath) {
+        if (auto unde = read_file(filepath)){
+                auto pe = parse_entry(unde.value());
+                if(!pe.has_value())
+                        return {};
+                return (DesktopEntry) {
+                        .pe = pe.value(),
+                };
+        } else {
+                return {};
+        }
 }
 
 }
--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
-#include "gtkmm/button.h"
-#include "gtkmm/window.h"
-#include "sigc++/functors/ptr_fun.h"
+//#include "sigc++/functors/ptr_fun.h"
 #include <iostream>
 #include <gtkmm.h>
 #include <format>
+#include <filesystem>
+#include <iostream>
+#include <unordered_map>
 
 #include "desktop.cpp"
+#include "glibmm/ustring.h"
+#include "gtkmm/liststore.h"
+#include "sigc++/functors/ptr_fun.h"
 
 struct ListUi {
 	Gtk::ListView *listem;
@@ -15,8 +19,8 @@ struct ListUi {
 };
 
 struct EntryUi {
-	std::string filepath;
-	Gtk::Label *filename_label, *exec_label;
+	deskentry::DesktopEntry de;
+	Gtk::Label *filename_label, *exec_label, *url_label;
 };
 
 struct KisayolApp {
@@ -25,6 +29,7 @@ struct KisayolApp {
 	Gtk::Window *main_window, *entry_window;
 	Gtk::Button *ab;
 	Gtk::Entry *etxt;
+	std::vector<deskentry::DesktopEntry> list;
 	ListUi lui;
 	EntryUi eui;
 };
@@ -33,8 +38,39 @@ struct KisayolApp {
 struct KisayolApp kapp = { 0 };
 
 static void lui_setup(const Glib::RefPtr<Gtk::ListItem>& list_item) {
-	GtkWidget *lb = gtk_label_new (NULL);
+	GtkWidget *lb = gtk_button_new ();
   	gtk_list_item_set_child (list_item->gobj(), lb);
+}
+
+
+void set_from_desktop_entry(EntryUi *eui, deskentry::DesktopEntry de) {
+
+	eui->filename_label->set_label(de.pe["Desktop Entry"]["Name"].strval);
+	eui->exec_label->set_label(de.pe["Desktop Entry"]["Exec"].strval);
+	eui->url_label->set_label(de.pe["Desktop Entry"]["GenericName"].strval);
+}
+
+void entry_ui() {
+	kapp.entry_window = kapp.builder->get_widget<Gtk::Window>("EntryWindow");
+	kapp.entry_window->set_hide_on_close(true);
+	kapp.entry_window->set_application(kapp.app);
+	kapp.eui.filename_label = kapp.builder->get_widget<Gtk::Label>("filename_label");
+	kapp.eui.exec_label = kapp.builder->get_widget<Gtk::Label>("exec_label");
+	kapp.eui.url_label = kapp.builder->get_widget<Gtk::Label>("url_label");
+	
+	set_from_desktop_entry(&kapp.eui, kapp.eui.de);
+	
+	kapp.entry_window->present();
+}
+
+void button_clicked(GtkWidget *widget, gpointer data) {
+	Glib::ustring strs = gtk_button_get_label(GTK_BUTTON(widget));
+	int pos = 0;
+	for(int i = 0; i < kapp.lui.strings.size(); i++)
+		if (kapp.lui.strings[i].data() == strs)
+			pos = i;
+	kapp.eui.de = kapp.list.at(pos == Glib::ustring::npos ? 0 : pos);
+	entry_ui();
 }
 
 static void lui_bind(const Glib::RefPtr<Gtk::ListItem>& list_item) {
@@ -42,7 +78,10 @@ static void lui_bind(const Glib::RefPtr<Gtk::ListItem>& list_item) {
 	/* Strobj is owned by the instance. Caller mustn't change or destroy it. */
 	GtkStringObject *strobj = GTK_STRING_OBJECT(gtk_list_item_get_item (list_item->gobj()));
 	/* The string returned by gtk_string_object_get_string is owned by the instance. */
-	gtk_label_set_text (GTK_LABEL (lb), gtk_string_object_get_string (strobj));
+	gtk_button_set_label (GTK_BUTTON(lb), gtk_string_object_get_string (strobj));
+	g_signal_connect(G_OBJECT(lb), "clicked", 
+      		G_CALLBACK(button_clicked), NULL);
+
 }
 
 static void add_button_clicked() {
@@ -52,23 +91,6 @@ static void add_button_clicked() {
 	kapp.etxt->set_text("");
 }
 
-void set_from_desktop_entry(EntryUi *eui, deskentry::DesktopEntry de) {
-	// TODO
-}
-
-void entry_ui() {
-	kapp.entry_window = kapp.builder->get_widget<Gtk::Window>("EntryWindow");
-	kapp.entry_window->set_application(kapp.app);
-	kapp.eui.filename_label = kapp.builder->get_widget<Gtk::Label>("filename_label");
-	kapp.eui.exec_label = kapp.builder->get_widget<Gtk::Label>("exec_label");
-	
-	if (auto de = deskentry::parse_file(kapp.eui.filepath)) {
-		set_from_desktop_entry(&kapp.eui, de.value());
-	}
-
-	kapp.entry_window->present();
-}
-
 void init_ui() {
 	const Gtk::TreeModelColumnRecord columns;
 	auto model = Gtk::ListStore::create(columns);
@@ -76,7 +98,13 @@ void init_ui() {
 	kapp.main_window = kapp.builder->get_widget<Gtk::Window>("main_window");
 	kapp.main_window->set_application(kapp.app);
 	
-	kapp.lui.strings.push_back("abc");
+	for (const auto & entry : std::filesystem::directory_iterator("/usr/share/applications/")){
+		auto pde = deskentry::parse_file(entry.path());
+		if(pde.has_value()) {
+			kapp.list.push_back(pde.value());
+			kapp.lui.strings.push_back(pde.value().pe["Desktop Entry"]["Name"].strval);
+		}
+	}
 	kapp.lui.sl = Gtk::StringList::create(kapp.lui.strings); // gtk_string_list_new ((const char * const *) array);
 	auto ns = Gtk::NoSelection::create(kapp.lui.sl); // gtk_no_selection_new (G_LIST_MODEL (sl));
 
@@ -111,8 +139,8 @@ int main(int argc, char* argv[])
 	if(!kapp.builder)
 		return 1;
 	
-	kapp.eui.filepath = "/home/mustafa/Desktop/xfce4-about.desktop";
-	kapp.app->signal_activate().connect(sigc::ptr_fun(entry_ui));
+	//kapp.eui.filepath = "/usr/share/applications/btop.desktop";
+	kapp.app->signal_activate().connect(sigc::ptr_fun(init_ui));
 
 	return kapp.app->run(argc, argv);
 }
